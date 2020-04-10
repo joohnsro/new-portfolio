0fb78fd16b67ec57562f56a6df1d7bdb0591dcda
Jonathan Oliveira <jonathansroliveira@gmail.com>
2020-04-10 19:17:37 -0300

    Inclusão de responsividade no menu

--- scripts.js
+++ scripts.js
@@ -1,13 +1,39 @@
 var content = document.querySelector('.content'),
     profileContainer = document.querySelector('.profile-container'),
     profileImage = document.querySelector('.profile-image'),
-    profileClose = document.getElementById('profile-close');
+    profileClose = document.getElementById('profile-close'),
+    mobileVersion = false;
+    header = document.querySelector('header'),
+    collapseMenu = document.getElementById('minimize-menu');
 
 profileImage.addEventListener('click', toggleProfile);
 profileClose.addEventListener('click', toggleProfile);
 
-function toggleProfile (){
+function toggleProfile() {
     var classList = profileContainer.classList;
     classList.toggle('open');
     content.classList.toggle('extended');
 }
+
+collapseMenu.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if ( !mobileVersion ) return;
+
+    var classList = header.classList;
+    classList.toggle('open');
+}
+
+function checkWindowWidth() {
+    mobileVersion = ( window.innerWidth <= 900 ) ? true : false;
+    
+    if (mobileVersion) {
+        header.classList.add('mobile');
+    } else {
+        header.classList.remove('mobile');
+    }
+}
+
+window.addEventListener('resize', checkWindowWidth);
+
+checkWindowWidth();
